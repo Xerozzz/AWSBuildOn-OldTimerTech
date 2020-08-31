094fd1729d19c29830c49f9d7801735411e668cd
LuJiaJun <52995265+Lu-JiaJun@users.noreply.github.com>
2020-09-01 01:13:45 +0800

    I am rartard

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import Nav from './global/Nav'
 
 function App() {
   return (
     <div className="App">
       <header>
-        <p>yeye</p>
         <h1>Auth is cool!</h1>
       </header>
       <AmplifySignOut />
--- src/components/global/Nav.js
+++ src/components/global/Nav.js
@@ -5,8 +5,18 @@ class Nav extends React.Component {
     render() {
         return(
             <nav>
-                
+                <h3>Old Timer Tech</h3>
+                <ul>
+                    <li>
+                        <NavLink to="/tutorial">Tutorials</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/voucher"></NavLink>
+                    </li>
+                </ul>
             </nav>
         )
     }
 }
+
+export default Nav;
