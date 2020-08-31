1b696e824c78dd3f305c76e031683cdcc2a3b22e
LuJiaJun <52995265+Lu-JiaJun@users.noreply.github.com>
2020-09-01 01:58:16 +0800

    Footer yeye

---
+++ src/components/global/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render() {
+        return(
+            <p>Copyright &#169 Calm Down La 2020</p>
+        )
+    }
+}
+
+export default Footer;
