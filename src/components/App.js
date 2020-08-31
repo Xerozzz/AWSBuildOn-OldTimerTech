d76b61e659d61ae7fb4775474f542b272c813202
LunarWiz <70055610+LunarWiz@users.noreply.github.com>
2020-08-31 21:41:52 +0800

    d d discommuncation

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import Nav from 'global/nav'
+import Switch from 'react-router-dom'
+import TutorialPage from 'tutorial/tutorialPage'
 
 function App() {
   return (
     <div className="App">
+      <Nav>
+
+      </Nav>
       <header>
         <p>yeye</p>
         <h1>Auth is cool!</h1>
       </header>
+      <Switch>
+        <Route path='tutorialsPage' component={TutorialPage}/>
+      </Switch>
       <AmplifySignOut />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import Amplify from 'aws-amplify';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import Amplify, { Auth } from 'aws-amplify';
 import config from './aws-exports';
-
-import { Provider } from 'react-redux';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
-import store from './store';
-
 Amplify.configure(config);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <Switch>
-        <Route path="/" component={App} />
-      </Switch>
-    </Router>
-  </Provider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
-
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
