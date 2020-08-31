591c7c7bec94b0d2b3c1adcbe542e2acb40105f1
LunarWiz <70055610+LunarWiz@users.noreply.github.com>
2020-08-31 23:04:40 +0800

    yes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-// import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 import Amplify, { Auth } from 'aws-amplify';
 import config from './aws-exports';
 Amplify.configure(config);
@@ -16,4 +16,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+serviceWorker.unregister();
