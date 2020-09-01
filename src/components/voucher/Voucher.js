60f62f98b0981f3633285fa8734b5ed74857a8fc
RyanOJX <RYANJX.20@ichat.sp.edu.sg>
2020-09-01 14:50:10 +0800

    Update Voucher.js

---
+++ src/components/voucher/Voucher.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+class Voucher extends React.Component{
+    render(){
+        return(
+            <table>
+                <tr>
+                    <th>Voucher Name</th>
+                    <th>Discount Amount (Off)</th>
+                </tr>
+                <tr>
+                    <td>NTUC Voucher</td>
+                    <td>$8</td>
+                </tr>
+                <tr>
+                    <td>NTUC Voucher</td>
+                    <td>$10</td>
+                </tr>
+                <tr>
+                    <td>Old Chang Kee Voucher</td>
+                    <td>$6</td>
+                </tr>
+            </table>
+        );
+    }
+}
+
+export default Voucher;
