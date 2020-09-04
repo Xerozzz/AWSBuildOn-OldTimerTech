844c59b2fb8f2d926c49daf60b93e0294f0d5332
LuJiaJun <52995265+Lu-JiaJun@users.noreply.github.com>
2020-09-04 16:52:16 +0800

    fixed some css

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,20 +5,23 @@ class Home extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <img id="oldman" src="static/css/img/oldmanyeye.jpg" alt="old man" />
-        <img id="aws" src="static/css/img/poweredbyaws.png" alt="aws" />
-        <header>
-          <p className="Jumbo">No need call ah boy liao</p>
-          <h1 className="Jumbo bold">Majulah!</h1>
-          <p>
-            E-payments made simple! With Old Timer Tech, you can learn how-to's
-            of e-payments and earn points by completing tutorials! Redeem
-            e-vouchers from NTUC, Old Chang Kee and More!
-          </p>
-          <button id="home-button">
-            {<Link to="/tutorial">Get started!</Link>}
-          </button>
-        </header>
+        <body>
+          <img id="oldman" src="static/css/img/oldmanyeye.jpg" alt="old man" />
+          <img id="aws" src="static/css/img/poweredbyaws.png" alt="aws" />
+
+          <header>
+            <p className="Jumbo">No need call ah boy liao</p>
+            <h1 className="Jumbo bold">Majulah!</h1>
+            <p>
+              E-payments made simple! With Old Timer Tech, you can learn
+              how-to's of e-payments and earn points by completing tutorials!
+              Redeem e-vouchers from NTUC, Old Chang Kee and More!
+            </p>
+            <button id="home-button">
+              {<Link to="/tutorial">Get started!</Link>}
+            </button>
+          </header>
+        </body>
       </React.Fragment>
     );
   }
